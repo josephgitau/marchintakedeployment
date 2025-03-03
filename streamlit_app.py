5284247fda0f75d05bc1d6172abf347ebac9d8ed
Joseph Gitau <gitaujoseph64@gmail.com>
2025-03-03 15:09:43 +0000

    text display info

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
+# import libraries
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# give your app a title
+st.title("Model Deployment Web app")
+
+# st.header
+st.header('Loan prediction Web app')
+
+# paragraphs of info
+st.write('This is a web created using streamlit for loan predictions. The app is supposed to take in user information and predict if an individual qualifies for a loan or not.')
+
+st.markdown('This is a project implemented by the march intake africdsa students. `streamlit web app`')
+
+st.subheader('Data Info')
+st.text('Some info on the loan data')
+
+# model formula
+st.text('Base model will be a linear regression equation with the formula shown below')
+st.latex(r"y = \beta_0 + \beta_1 x_1 + \beta_2 x_2 + \cdots + \beta_n x_n + \epsilon")
+st.caption('Equation 1: Linear regression equation')
